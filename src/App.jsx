0b761eb6b9e1f8caba5c34bab005a9a423d66cb0
hyunki <hyunki08@gmail.com>
2022-09-16 14:41:06 +0900

    라우터 구조 변경 및 nav 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import Nav from "./components/Nav";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
 import Weather from "./pages/Weather";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/weather/:region",
-    element: <Weather />,
-  },
-]);
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0px
+  }
+`;
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
+        <Route path="/weather/:region" element={<Weather />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const NavWrapper = styled.nav`
+  width: 100%;
+  height: 80px;
+  background-color: #aac4ff;
+  box-shadow: 1px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    height: 60px;
+  }
+`;
+
+const HomeButton = styled.a`
+  width: 200px;
+  padding: 20px;
+  font-size: 20px;
+  font-weight: 800;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+    padding: 10px;
+  }
+`;
+
+const Nav = () => {
+  const navigate = useNavigate();
+  const onClickHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <NavWrapper>
+      <HomeButton onClick={onClickHome}>Simple Weather App</HomeButton>
+    </NavWrapper>
+  );
+};
+
+export default Nav;
