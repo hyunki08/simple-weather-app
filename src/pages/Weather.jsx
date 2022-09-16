89abac5a1c65374e6f10a87640c68f60a642155c
hyunki <hyunki08@gmail.com>
2022-09-16 21:59:57 +0900

    날씨 정보 받아오는 기능 구현 및 테스트 데이터 추가

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import WeatherIcon from "../components/ui/WeatherIcon";
+
+const testData = {
+  temperature: "+28 °C",
+  wind: "4 km/h",
+  description: "Partly cloudy",
+  forecast: [
+    {
+      day: "1",
+      temperature: "+29 °C",
+      wind: "8 km/h",
+    },
+    {
+      day: "2",
+      temperature: "27 °C",
+      wind: "24 km/h",
+    },
+    {
+      day: "3",
+      temperature: " °C",
+      wind: "22 km/h",
+    },
+  ],
+};
+
+const WeatherWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 10px 20px;
+  gap: 10px;
+`;
 
 const Weather = () => {
-  return <div>Weather</div>;
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(true);
+  const param = useParams();
+
+  const fetchWeather = async () => {
+    try {
+      const { region } = param;
+      // const res = await fetch("http://localhost:5000/weather/" + region).then(res => res.json());
+      const res = testData;
+      console.log(res);
+      setWeather(res);
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!param || !param.region) return;
+    fetchWeather();
+  }, []);
+
+  return !loading ? (
+    <WeatherWrapper>
+      <WeatherIcon {...weather} size={100} />
+    </WeatherWrapper>
+  ) : (
+    <div>Loading...</div>
+  );
 };
 
 export default Weather;
