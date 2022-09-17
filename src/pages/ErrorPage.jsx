4b9c67892dca3bf5f3dcb5ba206ae60752a334de
hyunki <hyunki08@gmail.com>
2022-09-17 14:59:00 +0900

    에러 페이지 구현

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../components/ui/Button";
+
+const ErrorPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 10px 20px;
+  gap: 50px;
+`;
+
+const ErrorTitle = styled.div`
+  font-size: 50px;
+  font-weight: 600;
+
+  @media (max-width: 768px) {
+    font-size: 30px;
+  }
+`;
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+
+  const onClickGoHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <ErrorPageWrapper>
+      <ErrorTitle>페이지를 이용할 수 없습니다</ErrorTitle>
+      <Button width={"200px"} height={"50px"} onClick={onClickGoHome}>
+        홈으로 가기
+      </Button>
+    </ErrorPageWrapper>
+  );
 };
 
 export default ErrorPage;
